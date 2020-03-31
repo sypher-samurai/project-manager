297b7d745c66c0386c329cf68cf4e827b4765a48
sypher-samurai <spoiltcooker@gmail.com>
2020-03-31 11:07:42 +0100

    Team.vue improved

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -6,7 +6,13 @@
       <v-layout row wrap>
           <v-flex xs12 sm6 md4 lg3 v-for="team in teams" :key="team.name">
               <v-card flat="" class="text-xs-center ma-3">
-                  hfhfhhf
+                  <v-responsive class="pt-4">
+                    images goes here 
+                  </v-responsive>
+                  <v-card-text>
+                    <div class="subheading">{{ team.name}}</div>
+                    <div class="grey--text">{{ person.role}}</div>
+                  </v-card-text>
               </v-card>
           </v-flex>
       </v-layout>
