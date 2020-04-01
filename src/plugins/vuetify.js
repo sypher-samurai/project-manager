fe1be9093a16919a03f67a76200c4da5cdac9eed
sypher-samurai <spoiltcooker@gmail.com>
2020-04-01 15:11:54 +0100

    Avatar added

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,9 +5,20 @@ import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import "vuetify/dist/vuetify.min.css";
 import 'material-design-icons-iconfont'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+    theme: {
+      primary: '#3f51b5',
+      secondary: '#b0bec5',
+      accent: '#8c9eff',
+      error: '#b71c1c'
+    },
+    darkTheme: {
+      background: '#2e1a6c',
+    },
+  })
 Vue.use(VueRouter);
 
 export default new Vuetify({
+    
 });
 
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="team">
+  <div class="team" color="grey lighten-4">
     <h1 class="subheading grey--text">Team</h1>
 
     <v-container class="my-5">
-      <v-layout row wrap>
-          <v-flex xs12 sm6 md4 lg3 v-for="team in teams" :key="team.name">
-              <v-card flat="" class="text-xs-center ma-3">
-                  <v-responsive class="pt-4">
-                    images goes here 
-                  </v-responsive>
-                  <v-card-text>
-                    <div class="subheading">{{ team.name}}</div>
-                    <div class="grey--text">{{ person.role}}</div>
-                  </v-card-text>
-              </v-card>
-          </v-flex>
+      <v-layout row>
+        <v-flex xs12 sm6 md3  v-for="team in teams" :key="team.name">
+          <v-card flat="" class="text-center ma-3">
+            <v-responsive class="pt-4">
+              <v-avatar>
+                <img :src="team.avatar" alt="" />
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ team.name }}</div>
+              <div class="grey--text">{{ team.role }}</div>
+            </v-card-text>
+            
+              <v-card-actions>
+                <v-flex justify-center>
+                <v-btn text="" color="gray">
+                  <v-icon small="" left>message</v-icon>
+                  <span>message</span>
+                </v-btn>
+              </v-flex>
+              </v-card-actions>
+            
+          </v-card>
+        </v-flex>
       </v-layout>
     </v-container>
   </div>
@@ -27,23 +39,28 @@ export default {
       teams: [
         {
           name: "The net Ninja",
-          role: "Web developer"
+          role: "Web developer",
+          avatar: "/avartar-1.jpg"
         },
         {
           name: "Chu Li",
-          role: "Graphic designer"
+          role: "Graphic designer",
+          avatar: "/avartar-2.png"
         },
         {
           name: "Phonix",
-          role: "Web developer"
+          role: "Web developer",
+          avatar: "/avartar-3.jpg"
         },
         {
           name: "Goku",
-          role: "Social media marverick"
+          role: "Social media marverick",
+          avatar: "/avartar-4.png"
         },
         {
           name: "Yoshi",
-          role: "Sales guru"
+          role: "Sales guru",
+          avatar: "/avartar-5.png"
         }
       ]
     };
