71c794cf6e5e1c6241d4ca9f1e06dccf99555c50
sypher-samurai <spoiltcooker@gmail.com>
2020-04-04 17:39:03 +0100

    Progress

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -2,7 +2,7 @@
   <v-layout row justify-center>
     <v-dialog v-model="dialog" max-width="690">
       <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Add a New Project</v-btn>
+        <v-btn color="success" dark v-on="on">Add a New Project</v-btn>
       </template>
       <v-card>
         <v-card-title class="headline">Add a New Project</v-card-title>
@@ -16,6 +16,9 @@
               label="Information"
               prepend-icon="edit"
             ></v-textarea>
+            <v-btn class="success" @click="submit">
+                Add Project
+            </v-btn>
           </v-form>
         </v-card-text>
       </v-card>
@@ -32,5 +35,10 @@ export default {
       content:''
     };
   },
+  methods:{
+      submit(){
+          console.log(this.title ,  this.content);
+      }
+  }
 };
 </script>
